4acb056d48b87bbc61e3f788d6ee3f27fd7c1ad6
Karl Johnson <199382398+hirekarl@users.noreply.github.com>
2025-06-28 16:50:27 -0400

    Completed vehicle-purchase exercism challenge.

--- exercism/javascript/vehicle-purchase/vehicle-purchase.js
+++ exercism/javascript/vehicle-purchase/vehicle-purchase.js
@@ -5,13 +5,35 @@
 // implementing this exercise.
 
 /**
+ * A frozen, immutable array of vehicles that require a license.
+ *
+ * @constant
+ * @readonly
+ * @type {readonly string[]}
+ */
+const LICENSE_REQUIRED = Object.freeze(["car", "truck"]);
+
+/**
+ * A frozen, immutable enum-like Object to avoid magic numbers.
+ *
+ * @constant
+ * @readonly
+ * @type {Object.<string, number>}
+ */
+const DISCOUNT_RATES = Object.freeze({
+  FIFTY_PERCENT: 0.5,
+  SEVENTY_PERCENT: 0.7,
+  EIGHTY_PERCENT: 0.8,
+});
+
+/**
  * Determines whether or not you need a license to operate a certain kind of vehicle.
  *
  * @param {string} kind
  * @returns {boolean} whether a license is required
  */
 export function needsLicense(kind) {
-  throw new Error("Please implement the needsLicense function");
+  return LICENSE_REQUIRED.includes(kind);
 }
 
 /**
@@ -23,7 +45,10 @@ export function needsLicense(kind) {
  * @returns {string} a sentence of advice which option to choose
  */
 export function chooseVehicle(option1, option2) {
-  throw new Error("Please implement the chooseVehicle function");
+  const sorting = option1.localeCompare(option2);
+  const firstInAlphabet = sorting < 1 ? option1 : option2;
+
+  return `${firstInAlphabet} is clearly the better choice.`;
 }
 
 /**
@@ -35,5 +60,11 @@ export function chooseVehicle(option1, option2) {
  * @returns {number} expected resell price in the dealership
  */
 export function calculateResellPrice(originalPrice, age) {
-  throw new Error("Please implement the calculateResellPrice function");
+  if (age < 3) {
+    return originalPrice * DISCOUNT_RATES.EIGHTY_PERCENT;
+  } else if (age >= 3 && age <= 10) {
+    return originalPrice * DISCOUNT_RATES.SEVENTY_PERCENT;
+  } else {
+    return originalPrice * DISCOUNT_RATES.FIFTY_PERCENT;
+  }
 }
